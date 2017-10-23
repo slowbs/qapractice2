24dbf0aa6987e88c868ef2e598dfb360efc56b99
slowbs <poison_word@hotmail.com>
2017-10-23 17:59:15 +0700

    carousel bug resize on smartphone

--- qa/src/App.vue
+++ qa/src/App.vue
@@ -21,8 +21,8 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title" router
-                :to="item.link">
+        <v-btn flat v-for="item in menuItems" :key="item.title"
+        router :to="item.link">
           <v-icon left>{{ item.icon}}</v-icon>
           {{ item.title }}
         </v-btn>
@@ -47,5 +47,6 @@
         ]
       }
     }
+
   }
 </script>
--- qa/src/components/Home.vue
+++ qa/src/components/Home.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="center">
+  <div class="center" style="width:720px">
     <v-container>
       <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
         <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
         <v-flex xs12 sm6 class="text-xs-center text-sm-left">
-        <v-btn large router to="/meetup/new" class="info">Explore Meetups</v-btn>
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
       </v-flex>
       </v-layout>
+      <v-layout row wrap style="height:50vh">
+        <v-flex xs12>
+      <v-carousel hide-controls style="height:75%">
+        <v-carousel-item
+         v-for="(item,i) in items"
+         v-bind:src="item.src"
+         :key="i"></v-carousel-item>
+      </v-carousel>
+    </v-flex>
+  </v-layout>
     </v-container>
   </div>
-</tempalte>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: '/static/doc-images/carousel/squirrel.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/sky.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/bird.jpg'
+          },
+          {
+            src: '/static/doc-images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
